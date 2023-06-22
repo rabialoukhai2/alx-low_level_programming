5f776ffbd247a76f0a8db9491266476f5e3ebac0
rabialoukhai2 <rloukhi@gmail.com>
2023-06-22 04:43:19 -0700

    tasks

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _isupper - checks if parameter is an uppercase character.
+ * @c: input character.
+ * Return: 1 if is an uppercase character, 0 in other case.
+ */
+int _isupper(int c)
+{
+README.md if (c >= 65 && c <= 90)
+README.md {
+README.md README.md return (1);
+README.md }
+README.md else
+README.md {
+README.md README.md return (0);
+README.md }
+}
---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - prints numbers between 0 to 9
+ * (exclude 2 and  4).
+ * Return: Void.
+ */
+void print_most_numbers(void)
+{
+	int c;
+
+	for (c = 48; c < 58; c++)
+	{
+		if (c != 50 && c != 52)
+			_putchar(c);
+	}
+	_putchar('\n');
+}
