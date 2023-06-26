6def92ddb06d78d276314a7e090e8f611bfa8799
rabialoukhai2 <rloukhi@gmail.com>
2023-06-26 06:43:26 -0700

    commit task

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- *  takes a pointer to an int as parameter and 
- *  updates the value it points to to 98
+ * reset_to_98 - change the value of *n to 98.
+ * @n: pointer to address of n
  *  Return : Void.
  */
 void reset_to_98(int *n)
