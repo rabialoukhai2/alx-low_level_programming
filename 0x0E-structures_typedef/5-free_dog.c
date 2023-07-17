21b71fa158ed1371fc2a6aabd0a458af81272e74
rabialoukhai2 <rloukhi@gmail.com>
2023-07-17 05:41:36 -0700

    commit task

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -1,17 +1,18 @@
-
-#include <stdlib.h>
 #include "dog.h"
+#include <stdlib.h>
 
 /**
- * free_dog - frees memory allocated for a struct dog
- * @d: struct dog to free
+ * free_dog - frees dogs.
+ * @d: struct dog.
+ *
+ * Return: no return.
  */
 void free_dog(dog_t *d)
 {
-if (d)
-{
-free(d->name);
-free(d->owner);
-free(d);
-}
+	if (d)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
 }
